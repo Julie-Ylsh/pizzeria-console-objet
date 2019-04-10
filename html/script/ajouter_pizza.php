76f05da55da2bfdd770497d596164f46fa0fc127
julie <julie.bouaye@hotmail.fr>
2019-04-10 16:47:41 +0200

    TP html

---
+++ html/script/ajouter_pizza.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Julie's Pizza</title>
+</head>
+
+<body>
+
+<p>Bonjour !</p>
+
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['prenom']; ?> !</p>
+
+<p>Si tu veux changer de prénom, <a href="formulaire.php">clique ici</a> pour revenir à la page formulaire.php.</p>
+
+</body>
+</html>
